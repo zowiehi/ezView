a52828a2a318a6612976c8f61a27df939fa6c386
Zowie Haugaard <zdh27@nau.edu>
2016-12-07 20:53:20 -0500

    updated comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # ezView
+
+This program uses openGL and GLFW to render an image in an application window, and provides some basic tools for the viewing and manipulation of that image. To use this progrgam, please ensure that both openGL and GLFW are installed on your machine.
+
+
+##Usage
+
+
+To build the program, simply type ‘make’ and the project will build
+
+
+Use the format ./ezview image.ppm
+
+
+Please ensure that the provided .ppm file is a binary file (P6)
+
+##Controls
+
+E - Rotate the image to the left
+
+
+R - Rotate time image to the right
+
+
+A - Shear the image to the left
+
+
+S - Shear the image to the right
+
+
+Scroll - zoom the image in and out
+
+Arrow keys - pan the image up, down, left, or right
--- ezview.c
+++ ezview.c
@@ -10,7 +10,7 @@
 
 #define SIZE 256
 
-
+//global variables for paramaters changed by input callbacks
 float angle = 0;
 float scale = 1;
 float shear = 0;
@@ -25,21 +25,21 @@ typedef struct {
 
 // (-1, 1)  (1, 1)
 // (-1, -1) (1, -1)
-
+//Define the vertexes for the rectangle being drawn on
 Vertex vertexes[] = {
   {{1, -1},  {0.99999, 0.99999}},
   {{1, 1}, {0.99999, 0}},
   {{-1, -1}, {0, 0.99999}},
   {{-1, 1}, {0, 0}}
 };
-
+//Define the two triangles of the square
 GLuint indices[] = {
     0, 1, 3,   // First Triangle
     2, 0, 3     // Second Triangle
 };
 
 
-
+//vertex shader code
 static const char* vertex_shader_text =
 "uniform mat4 MVP;\n"
 "attribute vec2 TexCoordIn;\n"
@@ -51,6 +51,7 @@ static const char* vertex_shader_text =
 "    TexCoordOut = TexCoordIn;\n"
 "}\n";
 
+//fragment shader code
 static const char* fragment_shader_text =
 "varying vec2 TexCoordOut;\n"
 "uniform sampler2D Texture;\n"
@@ -59,51 +60,65 @@ static const char* fragment_shader_text =
 "    gl_FragColor = texture2D(Texture, TexCoordOut);\n"
 "}\n";
 
+//generic error handling
 static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
 }
 
+//handle all user input from the keyboard
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+    //if the escape key is pressed, close the window
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 
+    // Shear the image to the right with the 'S' key
       if (key == GLFW_KEY_S && action == GLFW_PRESS)
         shear += .05;
 
+    // Shear the image to the left with the 'A' key
       if (key == GLFW_KEY_A && action == GLFW_PRESS)
         shear -= .05;
 
+    // Pan the image to the left with the 'LEFT' key
       if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
         xTran += .05;
 
+    // Pan the image to the right with the 'RIGHT' key
       if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
         xTran -= .05;
 
+    // Pan the image down with the 'DOWN' key
       if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
         yTran += .05;
 
+    // Pan the image up with the 'UP' key
       if (key == GLFW_KEY_UP && action == GLFW_PRESS)
         yTran -= .05;
 
+    // Rotate the image to the right using the 'R' key
     if(key == GLFW_KEY_R && action == GLFW_PRESS){
       angle -= 1;
       if(angle >= 4) angle = 0;
     }
 
+  // Rotate the image to the left using the 'E' key
     if(key == GLFW_KEY_E && action == GLFW_PRESS ){
       angle += 1;
       if(angle <= -4) angle = 0;
     }
 }
 
+//handle zoom using a callback to the scroll
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
+  //scale the image using the y axis offset from the scroll
   scale += (float)yoffset / 100;
   if(scale <= 0) scale = 0;
 }
 
+// Program to handle the compiling of the shader, and upon failure the calling of an error and exit of the program
 void glCompileShaderOrDie(GLuint shader) {
   GLint compiled;
   glCompileShader(shader);
@@ -122,6 +137,8 @@ void glCompileShaderOrDie(GLuint shader) {
     exit(1);
   }
 }
+
+// Program to handle the linking of the program, and upon failure the calling of an error and exit of the program
 void glLinkProgramOrDie(GLuint program) {
   GLint linked;
   glLinkProgram(program);
@@ -141,17 +158,7 @@ void glLinkProgramOrDie(GLuint program) {
   }
 }
 
-void reverse(unsigned char* ar, int n){
-  char c;
-  for(int i = 0; i < n; i++){
-    c = ar[i];
-    ar[i] = ar[n];
-    ar[n] = c;
-    n--;
-  }
-  return;
-}
-
+// method used to handle the loading of the image to be viewed, in .ppm format
 unsigned char* loadImage(FILE* inFile, int* width, int* height){
   //buffer used for the comments mainly
   char buff[SIZE], *fh;
@@ -192,9 +199,8 @@ unsigned char* loadImage(FILE* inFile, int* width, int* height){
 
   fseek(inFile, 1, SEEK_CUR);
   image = (unsigned char *)malloc(sizeof(char)* 3 * w * h);
+  //Read the image data from the file
   fread(image, sizeof(unsigned char), w * h * 3, inFile);
-
-  //reverse(image, w * h * 3);
 
   *width = w;
   *height = h;
@@ -203,7 +209,7 @@ unsigned char* loadImage(FILE* inFile, int* width, int* height){
 
 int main(int argc, char *argv[])
 {
-
+  //Check for propper arguments
   if(argc < 2 || argc > 2) {
     perror("Usage: ./ezview image-source.ppm \n");
   }
@@ -213,82 +219,87 @@ int main(int argc, char *argv[])
     return 0;
   }
 
+  //open the image file
   FILE *inFile = fopen(argv[1], "rb");
 
   GLint image_width, image_height;
 
+  //read in the image file and store it
   GLubyte* image = (GLubyte*) loadImage(inFile, &image_width, &image_height);
-
-
 
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
     GLint mvp_location, vpos_location, vcol_location;
 
+
     glfwSetErrorCallback(error_callback);
 
+    //initialize glfw
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    window = glfwCreateWindow(image_width, image_height, "Simple example", NULL, NULL);
+    //create the glfw window, use the image to set width and height, and file name for title
+    window = glfwCreateWindow(image_width, image_height, argv[1], NULL, NULL);
     if (!window)
-    {
+    {   //terminate if unable to open
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
 
+    //set the callbacks for the input
     glfwSetKeyCallback(window, key_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
     glfwMakeContextCurrent(window);
-    // gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
     glfwSwapInterval(1);
 
-    // NOTE: OpenGL error checks have been omitted for brevity
-
+    //set up the element buffer object
     GLuint EBO;
     glGenBuffers(1, &EBO);
 
-    // GLuint VAO;
-    // glGenVertexArrays(1, &VAO);
-    // glBindVertexArray(VAO);
 
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //element buffer object used for indeces
     glBufferData(GL_ELEMENT_ARRAY_BUFFER,
     sizeof(indices), indices, GL_STATIC_DRAW);
 
+    //initialize the vertex shader
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShaderOrDie(vertex_shader);
 
+    //initialize the fragment shader
     fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
     glCompileShaderOrDie(fragment_shader);
 
+    // Create the program
     program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgramOrDie(program);
 
-
+    //set the mvp location from the vertex shader
     mvp_location = glGetUniformLocation(program, "MVP");
     assert(mvp_location != -1);
 
+    //set the vpos location from the vertex shader
     vpos_location = glGetAttribLocation(program, "vPos");
     assert(vpos_location != -1);
-
+    //set the texture coordinate location from the fragment shader
     GLint texcoord_location = glGetAttribLocation(program, "TexCoordIn");
     assert(texcoord_location != -1);
-
+    //set the texture location from the fragment shader
     GLint tex_location = glGetUniformLocation(program, "Texture");
     assert(tex_location != -1);
+
 
     glEnableVertexAttribArray(vpos_location);
     glVertexAttribPointer(vpos_location,
@@ -306,7 +317,7 @@ int main(int argc, char *argv[])
                           sizeof(Vertex),
 			  (void*) (sizeof(float) * 2));
 
-
+    //setup textures
     GLuint texID;
     glGenTextures(1, &texID);
     glEnable( GL_TEXTURE_2D );
@@ -323,32 +334,27 @@ int main(int argc, char *argv[])
     glBindTexture(GL_TEXTURE_2D, texID);
     glUniform1i(tex_location, 0);
 
+    //main program loop
     while (!glfwWindowShouldClose(window))
     {
-        float ratio;
+        GLfloat ratio;
         int width, height;
         mat4x4 m, p, mvp, s;
 
+        //matrix used for the shear operation
         mat4x4 sh = {
             1.0f, 0.0f, 0.0f, 0.0f,
             shear, 1.0f, 0.0f, 0.0f,
             0.0f, 0.0f, 1.0f, 0.0f,
             0.0f, 0.0f, 0.0f, 1.0f
         };
-
+        //matrix used for the zoom operation
         mat4x4 zoom = {
             scale, 0.0f, 0.0f, 0.0f,
             0.0, scale, 0.0f, 0.0f,
             0.0f, 0.0f, 1.0f, 0.0f,
             0.0f, 0.0f, 0.0f, 1.0f
         };
-        mat4x4 tran = {
-            1.0f, 0.0f, 0.0f, xTran,
-            0.0, 1.0f, 0.0f, yTran,
-            0.0f, 0.0f, 1.0f, 1.0f,
-            0.0f, 0.0f, 0.0f, 1.0f
-        };
-
 
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
@@ -356,22 +362,24 @@ int main(int argc, char *argv[])
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        //set up matrix for transformation
         mat4x4_identity(m);
-        mat4x4_identity(s);
+        //apply shear
         mat4x4_mul(m, sh, m);
+        //apply zoom
         mat4x4_mul(m, zoom, m);
+        //apply translate
         mat4x4_translate_in_place(m, xTran, yTran, 1.0);
-        //mat4x4_add(m, tran, m);
+        //apply rotate
         mat4x4_rotate_Z(m, m, (angle * M_PI/2));
-        //mat4x4_scale(s,s, scale);
 
         mat4x4_identity(p);
-        //mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, s, m);
+        //apply all transformations
+        mat4x4_mul(mvp,p, m);
 
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
-
+        //draw the updated geometry to the screen
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
@@ -379,7 +387,7 @@ int main(int argc, char *argv[])
     }
 
     glfwDestroyWindow(window);
-
+    //exit
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
